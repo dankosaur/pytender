dd28e0184062734687c66ccdc800ff12ac92416a
dan kuebrich <dan.kuebrich@gmail.com>
2011-05-02 18:43:04 +0000

    [setup] make installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name = 'tender',
+      version = '0.2',
+      author = 'A few different folks on github',
+      author_email = 'dan@tracelytics.com',
+      url = 'http://github.com/dankosaur/pytender',
+      download_url = 'http://github.com/dankosaur/pytender',
+      description  = 'Python TenderApp Client',
+      license = '?',
+      install_requires = ['tpg>=3.1.2']
+      )
